50f94f7dbe2610df0a0a698e684aff31429bbfc5
Daniel8054 <jdhurtado@fi.uba.ar>
2024-05-02 23:33:48 -0300

    Daneil Cambio 1

--- diccionario/dicccionario_hash.go
+++ diccionario/dicccionario_hash.go
@@ -1,14 +1,40 @@
 package diccionario
 
-type diccionarioHash[K comparable, V any] struct {
-	//Estructura del hash definir si es abierto o cerrado y como
+type estadoNodo int
+
+const (
+	VACIO = estadoNodo(iota)
+	OCUPADO
+	BORRADO
+)
+
+const LARGOINICIAL int = 13
+
+type nodoHash[K comparable, V any] struct {
+	clave  K
+	dato   V
+	estado estadoNodo
+}
+
+type hashCerrado[K comparable, V any] struct {
+	contenido []nodoHash[K, V]
+	tamaño    int
+	cantidad  int
+	borrados  int
 }
 
 type iterDiccionarioHash[K comparable, V any] struct {
+	tabla    *[]nodoHash[K, V]
+	posicion int
+	//Este iterador se debe mover hasta que encuentre algo
 }
 
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
-
+	nuevo := new(hashCerrado[K, V])
+	nuevo.contenido = make([]nodoHash[K, V], LARGOINICIAL)
+	nuevo.tamaño = LARGOINICIAL
+	nuevo.cantidad, nuevo.borrados = 0, 0
+	return nuevo
 }
 
 //Todas las funciones van a tener
@@ -17,25 +43,25 @@ func CrearHash[K comparable, V any]() Diccionario[K, V] {
 }
 */
 //Primitivas del Hash
-func (hash *diccionarioHash[K, V]) Guardar(clave K, dato V) {
+func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 
 }
-func (hash *diccionarioHash[K, V]) Pertenece(clave K) bool {
+func (hash *hashCerrado[K, V]) Pertenece(clave K) bool {
 
 }
-func (hash *diccionarioHash[K, V]) Obtener(clave K) V {
+func (hash *hashCerrado[K, V]) Obtener(clave K) V {
 
 }
-func (hash *diccionarioHash[K, V]) Borrar(clave K) V {
+func (hash *hashCerrado[K, V]) Borrar(clave K) V {
 
 }
-func (hash *diccionarioHash[K, V]) Cantidad() int {
+func (hash *hashCerrado[K, V]) Cantidad() int {
 
 }
-func (hash *diccionarioHash[K, V]) Iterar(func(clave K, dato V) bool) {
+func (hash *hashCerrado[K, V]) Iterar(func(clave K, dato V) bool) {
 
 }
-func (hash *diccionarioHash[K, V]) Iterador() IterDiccionario[K, V] {
+func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
 
 }
 
